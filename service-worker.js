7195a013d7437ad0be320084327c1e728919c1b3
LOJAPP <jacarandalogin@gmail.com>
2026-07-19 03:12:54 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='jac-20260719_0100';
+const CACHE='jac-20260719_0300';
 const ASSETS=['./','./index.html','./jacaranda_manutencao.html','./jacaranda_resposta.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
