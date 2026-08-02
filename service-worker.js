c1862b48aaf667b4431839c859c3717a8a7c11e8
LOJAPP <jacarandalogin@gmail.com>
2026-08-01 23:14:39 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,5 @@
-const CACHE='jac-20260731_0900';
-const ASSETS=['./','./index.html','./jacaranda_manutencao.html','./jacaranda_resposta.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='jac-20260801_1000';
+const ASSETS=['./','./index.html','./jacaranda_manutencao.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  const isHTML = e.request.mode==='navigate' || (e.request.headers.get('accept')||'').includes('text/html');
-  if(isHTML){
-    e.respondWith(fetch(e.request).then(r=>{if(r.ok)caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r;}).catch(()=>caches.match(e.request)));
-  } else {
-    e.respondWith(caches.match(e.request).then(cached=>{const net=fetch(e.request).then(r=>{if(r.ok)caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r;}).catch(()=>cached);return cached||net;}));
-  }
-});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.open(CACHE).then(c=>c.match(e.request).then(r=>r||fetch(e.request).then(res=>{c.put(e.request,res.clone());return res;}))));});
